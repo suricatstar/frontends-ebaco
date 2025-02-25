fcff37a1545fa3681f64bb4c7e689a404258a7a1
Cauê Tamiarana <caue.tamiarana@gmail.com>
2025-02-25 00:43:01 -0300

    refatorar estilos e estrutura do projeto, removendo arquivos CSS desnecessários e adicionando novos estilos

--- grunt/gruntfile.js
+++ grunt/gruntfile.js
@@ -6,12 +6,12 @@ module.exports = function(grunt) {
        less: {
             development: {
                 files: {
-                    'main.css' : "main.less"
+                    'dev/styles/main.css' : "src/styles/main.less"
                 }
             },
             production: {
                 files: {
-                    'main.min.css' : "main.less"
+                    'dist/styles/main.min.css' : "src/styles/main.less"
                 },
                 options: {
                     compress: true,
@@ -20,32 +20,42 @@ module.exports = function(grunt) {
                 }
             }
        },
-       sass: {
-            dist: {
-                option: {
-                    style: 'compressed'
-                },
-                files: {
-                    'main2.css' : 'main.scss'
-                }
-            }
-       },
-       concurrent: {
-              target: ['olaGrunt','less', 'sass']
-       }
+         watch: {
+              styles: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development'],
+                
+              }
+         },
+    //    sass: {
+    //         dist: {
+    //             option: {
+    //                 style: 'compressed'
+    //             },
+    //             files: {
+    //                 'main2.css' : 'main.scss'
+    //             }
+    //         }
+    //    },
+    //    concurrent: {
+    //           target: ['olaGrunt','less', 'sass']
+    //    }
     })
 
-    grunt.registerTask('olaGrunt', function() {
-        const done = this.async();
-        setTimeout(function() {
-            console.log('Hello World');
-            done();
-        }, 3000);
-    })
+    // grunt.registerTask('olaGrunt', function() {
+    //     const done = this.async();
+    //     setTimeout(function() {
+    //         console.log('Hello World');
+    //         done();
+    //     }, 3000);
+    // })
     
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-concurrent');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    // grunt.loadNpmTasks('grunt-contrib-sass');
+    // grunt.loadNpmTasks('grunt-concurrent');
 
-    grunt.registerTask('default', ['concurrent']);
+    // grunt.registerTask('default', ['concurrent']);
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['less:production']);
 }
